d43c702c123162d8d280e2e7574ebdd4f82940ad
andyhyshi <57511290+andyhyshi@users.noreply.github.com>
2020-04-06 10:47:54 -0500

    all

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,100 +1,101 @@
-// @TODO: YOUR CODE HERE!
-// Set up chart
-
 var svgWidth = 960;
 var svgHeight = 500;
 
 var margin = {
-  top: 20,
-  right: 40,
-  bottom: 60,
-  left: 100
+    top: 20,
+    right: 40, 
+    bottom: 60,
+    left: 40
 };
 
 var width = svgWidth - margin.left - margin.right;
 var height = svgHeight - margin.top - margin.bottom;
 
-// Create an SVG wrapper, append an SVG group that will hold our chart, and shift the latter by left and top margins.
-var svg = d3.select(".chart")
-  .append("svg")
-  .attr("width", svgWidth)
-  .attr("height", svgHeight);
+// create svg wrapper, append an svg group to hold chart, shift the group by margins
+var svg = d3.select("#scatter")
+    .append("svg")
+    .attr("width", svgWidth)
+    .attr("height", svgHeight)
 
 var chartGroup = svg.append("g")
-  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+    .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
-// d3.select("#scatter").append ("div").attri("class", "tooltip").style("opacity", 0);  
-
-// Import Data
-d3.csv("/data/data.csv").then(function(riskFactor) {
-
-    //Parse Data/Cast as numbers
-    riskFactor.forEach(function(data) {
-      data.income = +data.income;
-      data.obesity = +data.obesity;
+// import data
+d3.csv("D3_data_journalism/assets/data/data.csv").then(function(incomeData) {
+    
+    // parse data
+    incomeData.forEach(function(data) {
+        data.poverty = +data.poverty;
+        data.healthcare = +data.healthcare;
     });
 
-    //Create scale functions
-    var xLinearScale = d3.scaleLinear().range([0, width]);
+    //create scale functions
+    var xLinearScale = d3.scaleLinear()
+        .domain([d3.min(incomeData, d => d.poverty)-1, d3.max(incomeData, d => d.poverty)])
+        .range([0, width]);
 
-    var yLinearScale = d3.scaleLinear().range([height, 0]);
+    var yLinearScale = d3.scaleLinear()
+        .domain([d3.min(incomeData, d => d.healthcare)-1, d3.max(incomeData, d => d.healthcare)])
+        .range([height, 0]);
 
-    //Create axis functions
+    // create axis functions
     var bottomAxis = d3.axisBottom(xLinearScale);
     var leftAxis = d3.axisLeft(yLinearScale);
 
-    // Append Axes to the chart
+    // append axis to the chart
     chartGroup.append("g")
-      .attr("transform", `translate(0, ${height})`)
-      .call(bottomAxis);
+        .attr("transform", `translate(0, ${height})`)
+        .call(bottomAxis);
 
     chartGroup.append("g")
-      .call(leftAxis);
+        .call(leftAxis);
 
-    // Create Circles
-    var circlesGroup = chartGroup.selectAll("circle")
-    .data(riskFactor)
-    .enter()
-    .append("circle")
-    .attr("cx", d => xLinearScale(d.income))
-    .attr("cy", d => yLinearScale(d.obesity))
-    .attr("r", "15")
-    .attr("fill", "pink")
-    .attr("opacity", ".5");
-
-    // initialize tool tip
+    // create circles    
+    var circlesGroup = chartGroup.selectAll("g circle")
+        .data(incomeData)
+        .enter()
+        .append("circle")
+        .attr("class", "stateCircle")
+        .attr("cx", d => xLinearScale(d.poverty))
+        .attr("cy", d => yLinearScale(d.healthcare))
+        .attr("r", "12")
+        .attr("stroke", "black")
+        .attr("opacity", ".60")
+    
+    // initialize tooltip
     var toolTip = d3.tip()
-      .attr("class", "tooltip")
-      .offset([80, -60])
-      .html(function(d) {
-        return (abbr + "%");
-      });
-
-    // Create tooltip in the chart
+        .attr("class", "d3-tip")
+        .offset([80, -60])
+        .html(function(d) {
+            return (`${d.abbr}<br>In Poverty (%): ${d.poverty}<br>Lacks Healthcare (%): ${d.healthcare}`);
+        });
+    
+    // create tooltip in the chart
     chartGroup.call(toolTip);
 
-    //Create event listeners to display and hide the tooltip
-    circlesGroup.on("click", function(data) {
-      toolTip.show(data, this);
+    // create event listeners
+    circlesGroup.on("mouseover", function(data) {
+        toolTip.show(data, this);
     })
-      // onmouseout event
-      .on("mouseout", function(data, index) {
-        toolTip.hide(data);
-      });
 
-    // Create axes labels
+        // on mouseout event
+        .on("mouseout", function(data, index) {
+            toolTip.hide(data);
+        });
+    
+    // create axis labels
     chartGroup.append("text")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 0 - margin.left + 40)
-      .attr("x", 0 - (height / 2))
-      .attr("dy", "1em")
-      .attr("class", "axisText")
-      .text("Obesity");
+        .attr("transform", "rotate(-90)")
+        .attr("y", 0 - margin.left + 0)
+        .attr("x", 200 - height)
+        .attr("dy", "1em")
+        .attr("class", "axisText")
+        .text("Lacks Healthcare (%)");
 
     chartGroup.append("text")
-      .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
-      .attr("class", "axisText")
-      .text("Income (Money Brought Home))");
-  }).catch(function(error) {
+        .attr("transform", `translate(${width / 2}, ${height + margin.top + 12})`)
+        .attr("class", "axisText")
+        .text("In Poverty (%)");
+}).catch(function(error) {
     console.log(error);
 });
